909282f6c5d4767d1a2905fa5c55faa45bd0780b
Postmedias <vincenthure@mac.com>
2019-01-10 22:01:05 +0100

    initial

---
+++ bleno.js
@@ -0,0 +1,5 @@
+[Desktop Entry]
+Type=Link
+Name=bleno.js
+Icon=application-javascript
+URL=sftp://pilote.local/home/pi/bleno_app/bleno.js
---
+++ pilote.js
@@ -0,0 +1,5 @@
+[Desktop Entry]
+Type=Link
+Name=pilote.js
+Icon=application-javascript
+URL=sftp://pilote.local/home/pi/bleno_app/pilote.js
